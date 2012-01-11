4f7b7a7cad3bdb4dff44ba5e0b63da7277e559b7
Brian Buchalter <bal711@gmail.com>
2012-01-11 13:35:35 -0400

    Added connection error handling.  Style updates.

--- check_mysql_replication.sh
+++ check_mysql_replication.sh
@@ -6,7 +6,6 @@ STATE_WARNING=1
 STATE_CRITICAL=2
 STATE_UNKNOWN=3
 STATE_DEPENDENT=4
-
 REPL_DIFFERENCE=1
 
 usage1="
@@ -16,7 +15,8 @@ Usage: $0 -H <slave_host>
 
 NOTE: Script assumes:
  * it is run from the master server
- * ~/.my.cnf is configured with account which has privileges to SHOW MASTER STATUS and SHOW MASTER STATUS."
+ * ~/.my.cnf is configured with account which has privileges to SHOW MASTER STATUS and SHOW MASTER STATUS.
+"
 
 if [ -z $1 ]
 then
@@ -49,14 +49,25 @@ while true; do
     test -n "$1" || break
 done
 
+slave_connection_check=`mysql -h $SLAVEIP_1 -e "show slave status" 2>&1`
+if [[ $slave_connetion_check == *ERROR* ]]
+then
+echo "Error reading slave: $slave_connection_check"
+exit $STATE_UNKNOWN
+fi
+
+
+master_connection_check=`mysql -e "show master status" 2>&1`
+if [[ $master_connection_check == *ERROR* ]];
+then
+echo "Error reading master: $master_connection_check"
+exit $STATE_UNKNOWN
+fi
 
 iSlave_1_position=`mysql -h $SLAVEIP_1 -e "show slave status" | grep bin | cut -f7`
-
 iSlave_1_status=`mysql -h $SLAVEIP_1 -e "show slave status" | grep bin | cut -f1`
-
-iMaster=`mysql -e "show master status" | grep bin | cut -f2`
-
-iDiff_1=`expr $iMaster - $iSlave_1_position`
+iMaster_position=`mysql -e "show master status" | grep bin | cut -f2`
+iDiff_1=`expr $iMaster_position - $iSlave_1_position`
 
 if [ $iDiff_1 -gt $REPL_DIFFERENCE ]
 then
@@ -67,6 +78,6 @@ then
 echo "CRITICAL - slave status is '$iSlave_1_status'"
 exit $STATE_CRITICAL
 else
-echo "OK - log positions match ($iMaster == $iSlave_1_position), slave status = '$iSlave_1_status'"
+echo "OK - log positions match ($iMaster_position == $iSlave_1_position), slave status = '$iSlave_1_status'"
 exit $STATE_OK
 fi
